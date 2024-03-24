9c68fc7fef39600ae37e6e1365dd80af33298293
eb-jramirez <106706282+eb-jramirez@users.noreply.github.com>
2024-03-24 13:33:17 -0700

    init commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+using Steeltoe.Connector.PostgreSql;
+using Steeltoe.Extensions.Logging;
+using Steeltoe.Management.Endpoint;
+
+var builder = WebApplication.CreateBuilder(args)
+;
+
+builder.Logging.AddDynamicConsole();
+
+builder.Services.AddPostgresConnection(builder.Configuration);
+builder.Services.AddAllActuators(builder.Configuration);
+builder.Services.ActivateActuatorEndpoints();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast").WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
